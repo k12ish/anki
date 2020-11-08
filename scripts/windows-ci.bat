24eead7192dd646294a46eff2448ad55a7ec8977
Damien Elmes <gpg@ankiweb.net>
2020-11-08 20:20:29 +1000

    try temp build folder

--- scripts/windows-ci.bat
+++ scripts/windows-ci.bat
@@ -1,7 +1,8 @@
 ECHO on
-set BAZEL=\bazel\bazel.exe --output_user_root=\bazel\anki --output_base=\bazel\anki\base
+set BAZEL=\bazel\bazel.exe --output_user_root=d:\bazel\anki --output_base=d:\bazel\anki\base 
+set BUILDARGS=--config=ci --disk_cache=c:\bazel\disk --repository_cache=c:\bazel\repo
 REM rollup will fail on the first build
-call %BAZEL% build --config=ci ... -k
+call %BAZEL% build %BUILDARGS% ... -k
 
-call %BAZEL% test --config=ci ...
+call %BAZEL% test %BUILDARGS% ...
 IF %ERRORLEVEL% NEQ 0 exit /B 1
